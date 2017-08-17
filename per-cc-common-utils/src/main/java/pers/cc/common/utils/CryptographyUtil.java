70b6d1611123c6612f1588db92ae627eb04c4602
cc <532224524@qq.com>
2017-08-17 23:54:56 +0800

    add test db_blog.sql

--- per-cc-common-utils/src/main/java/pers/cc/common/utils/CryptographyUtil.java
+++ per-cc-common-utils/src/main/java/pers/cc/common/utils/CryptographyUtil.java
@@ -9,17 +9,19 @@ import org.apache.shiro.crypto.hash.Md5Hash;
  *
  */
 public class CryptographyUtil {
-	/**
-	 * Md5����
-	 * @param str
-	 * @param salt
-	 * @return
-	 */
-	public static String md5(String str,String salt){
-		return new Md5Hash(str,salt).toString();
-	}
-	
-	public static void main(String[] args) {
-		System.out.println(CryptographyUtil.md5("123456", "cc"));
-	}
+    /**
+     * Md5����
+     * 
+     * @param str
+     * @param salt
+     * @return
+     */
+    public static String md5(String str, String salt) {
+        return new Md5Hash(str, salt).toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(CryptographyUtil.md5("admin", "cc"));
+    }
+
 }
