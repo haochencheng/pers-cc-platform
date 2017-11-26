f25230eb6a002485a07c110a339c3a8acc040f50
Administrator <Administrator@FZGOJMP5UEO9I3W>
2017-11-26 13:50:02 +0800

    。

--- pers-platform-monitor-web/src/test/java/pers/monitor/platform/web/Test.java
+++ pers-platform-monitor-web/src/test/java/pers/monitor/platform/web/Test.java
@@ -1,5 +1,12 @@
-package pers.monitor.platform.web;  
-  
+package pers.monitor.platform.web;
+
+import org.springframework.boot.context.embedded.WebApplicationContextServletContextAwareProcessor;
+
 public class Test {
+
+
+    public static void main(String[] args) {
+        WebApplicationContextServletContextAwareProcessor webApplicationContextServletContextAwareProcessor;
+    }
 
 }
